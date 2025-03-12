fa6b78ae8213ba754bef866b869eb4de9620084e
simon <sbmp2000@gmail.com>
2025-03-11 22:34:10 -0700

    solution for dining philosophers

--- 4/4.py
+++ 4/4.py
@@ -45,10 +45,42 @@ They would all wait for their left fork to be dropped, but no forks will ever be
 
 Fix the deadlock:
 
+We deadlock here because each person grabs for the right fork and then the left. What happens if some go the other way around?
+
+
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+get_forks(i):
+  if i%2 == 0: #left handed
+    left(i).wait()
+    right(i).wait()
+  else: #right handed
+    right(i).wait()
+    left(i).wait()
 
+drop_forks(i): #order doesn't matter when releasing
+  right(i).signal()
+  left(i).signal()
 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Let's imagine the same scenario as before, each philosopher trying to get_forks() simultaneously. Where before each one would grab
+the one on their left and wait for the one on their right, now even ones grab the left and odd the right. This gives us some 
+contention:
+  - 0 and 1 try to grab fork 1
+    - one of them will grab it
+  - 2 and 3 try to grab fork 3
+    - one grabs it
+  - 4 grabs fork 0
+
+At this point we are still able to proceed.
+  - It's possible for multiple philosophers to eat. Assuming 0 grabs f.1, 2 grabs f.3.
+    - then philosopher 4 will grab fork 4 and eat and philosopher 2 will grab fork 2 and eat
+  - I don't believe there's any deadlock here, as when philosophers are fighting over a fork, there will be a winner
+  - What about starvation?
+    - i don't think it can happen here. because the philosophers are fighting over forks, the instant one puts one down, the other
+    will grab it. There is no opportunity for the philosopher to pick the fork back up before the other grabs it. FIFO ?
+    - there is only ever 1 thread waiting for any given fork, so it will get that fork when it is put down.
 
 
 '''
